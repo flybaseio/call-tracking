b3d9fba9d3411457b277ef186bcab0c27c556f7d
Roger Stringer <roger@datamcfly.com>
2016-09-10 02:32:24 -0400

    first

---
+++ dashboard/dashboard.js
@@ -0,0 +1,50 @@
+$( function() {
+	var calls = $('#calls').epoch( {
+		type: 'time.area', axes: ['left', 'bottom', 'right'],
+		data: [ { values: [ { time: Date.now()/1000, y: 0 } ] } ]
+	} );
+	var numbers = $( '#numbers' ).epoch( { type: 'bar' } );
+	var cities = $( '#cities' ).epoch( { type: 'bar' } );
+	var stats = {
+		cities: {},
+		numbers: {}
+	};
+
+	var dashboard = new Flybase("YOUR-FLYBASE-API-KEY", "calltracking", "stats");
+
+	dashboard.once('value', function (data) {
+		updateStats( data );
+	});
+
+	dashboard.on( 'added', function (data ){
+		updateStats( data );
+	});
+
+	function updateStats( data ){
+		//	process the new data...
+		calls.push( [ { time: Date.now()/1000, y: data.count() } ] );
+
+		data.forEach( function( snapshot ){
+			var row = snapshot.value();
+			var cityCount = stats.cities[ row.city ] || 0;
+			stats.cities[ row.city ] = ++cityCount;
+
+			var numberCount = stats.numbers[ row.number ] || 0;
+			stats.numbers[ row.number ] = ++numberCount;
+		});	
+
+		var citiesData = [];
+		for( var city in stats.cities ) {
+			citiesData.push( { x: city, y: stats.cities[ city ] } );
+		}
+		cities.update( [ { values: citiesData } ] );
+
+		var numbersData = [];
+		for( var number in stats.numbers ) {
+			numbersData.push( { x: number, y: stats.numbers[ number ] } );
+		}
+		numbers.update( [ { values: numbersData } ] );
+
+	}
+
+});
---
+++ index.js
@@ -0,0 +1,65 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var flybase = require('flybase');
+var path = require('path');
+
+var cors = require('cors');
+var compression = require('compression');
+var serveStatic = require('serve-static');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({	extended: true	}));
+app.use(express.static( path.join(__dirname, 'public')));
+ 
+var port = process.env.PORT || 5000; // set our port
+
+var flybaseRef = flybase.init('YOUR-FLYBASE-APP-NAME', "calltracking", 'YOUR-FLYBASE-API-KEY');
+
+//	backend
+
+app.post('/call', function(req, res) {
+	flybaseRef.push({
+		number: req.body.To,
+		city: req.body.FromCity
+	}).then( function( rec ){
+		res.type('text/xml');
+		res.render('twiml', { message: 'Your call has been recorded!' })
+	}, function(err){
+		res.type('text/xml');
+		console.log(error);
+		res.render('twiml', { message: 'Sorry, an error happened.' });
+	});
+});
+
+
+app.get('/twiml', function(req, res) {
+	res.type('text/xml');
+	res.render('twiml', { message: 'Sorry, an error happened.' });
+});
+
+// frontend
+
+function setCustomCacheControl(res, path) {
+	if (serveStatic.mime.lookup(path) === 'text/html') {
+		// Custom Cache-Control for HTML files
+		res.setHeader('Cache-Control', 'public, max-age=0')
+	}
+}
+
+app.use(compression());
+
+app.use(serveStatic(__dirname + '/dashboard', { 
+	maxAge: '1d', 
+	setHeaders: setCustomCacheControl,
+	'index': ['index.html'],
+	fallthrough: true
+}));
+
+
+var server = http.createServer(app);
+server.listen(process.env.PORT || 3000, function() {
+	console.log('Express server started.');
+});
